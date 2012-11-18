c86799136804db848d5fc2d6e0a5695cd2fdaa81
Arunram Kalaiselvan <arun@edengroups.com>
2012-11-18 12:51:06 -0500

    updating camera icon

--- app/assets/javascripts/asset.js
+++ app/assets/javascripts/asset.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
 		$('#asset_lat').val(location.coords.latitude);
 		$('#asset_lng').val(location.coords.longitude);
 	}
+
+	$('#new_asset').bind('submit', function(){
+		$(this).find('input[type="submit"]').attr('disabled', 'disabled').attr('value', 'Uploading...');
+	});
 });
