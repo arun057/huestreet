bccaeac63fbeb91082dc717fba358da3e515f522
Arunram Kalaiselvan <arun@edengroups.com>
2012-11-18 13:27:56 -0500

    image wont go withouth image

--- app/assets/javascripts/asset.js
+++ app/assets/javascripts/asset.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
 		$('#asset_lng').val(location.coords.longitude);
 	}
 
-	$('#new_asset').bind('submit', function(){
+	$('#new_asset').bind('submit', function(e){
+		if(!$('#image_uploader').val().length){
+			e.preventDefault();
+			return false;
+		}
 		$(this).find('input[type="submit"]').attr('disabled', 'disabled').attr('value', 'Uploading...');
 	});
 });
